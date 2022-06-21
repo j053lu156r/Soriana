a1d88b5cfb7bde1143e3ecb7d8c1f2f3199fab5c
Opulido <opulido@strategy-a.com>
2022-06-21 09:46:36 -0500

    estado de cuenta fix
    ajustes estado de cueta

--- webapp/controllers/PaymentPlan/Master.controller.js
+++ webapp/controllers/PaymentPlan/Master.controller.js
@@ -14,8 +14,21 @@ sap.ui.define([
 ], function (jQuery, Fragment, Controller, UploadCollectionParameter, History, PDFViewer, JSONModel, fioriLibrary) {
     "use strict";
 
+    var sUri = "/sap/opu/odata/sap/ZOSP_STATEMENT_SRV_01/";
+    var dTJSON;
+    var fechaAct = new Date();
     var oModel = new this.PaymentPlan();
     return Controller.extend("demo.controllers.PaymentPlan.Master", {
+
+        sCollection: "GroupedTotales>/Hierarchy",
+        aCrumbs: ["movimientos", "positions"],
+        mInitialOrderState: {
+            products: {},
+            count: 0,
+            hasCounts: false
+        },
+
+
         onInit: function () {
             this.getView().addEventDelegate({
                 onAfterShow: function (oEvent) {
@@ -26,15 +39,25 @@ sap.ui.define([
                     this.getView().byId("datePay").setValue("");
                 }
             }, this);
+
+            if (!this._pTemplate) {
+                this._pTemplate = Fragment.load({
+                    id: this.getView().getId(),
+                    name: "demo.views.Statement.Row"
+                });
+            }
+            this._oTable = this.byId("idGroupTable");
         },
-        searchData: function () {
+        _searchData: function () {
             var bContinue = false;
 
             if (!oModel.getModel()) {
                 oModel.initModel();
             }
 
-            var formater = sap.ui.core.format.DateFormat.getDateTimeInstance({ parent: "yyyyMMdd" });
+            var formater = sap.ui.core.format.DateFormat.getDateTimeInstance({
+                parent: "yyyyMMdd"
+            });
             var dateRange = this.getView().byId("datePay");
 
             //Fechas de engrega
@@ -42,7 +65,7 @@ sap.ui.define([
             var endDate = this.buildSapDate(dateRange.getSecondDateValue());
 
             var vLifnr = this.getConfigModel().getProperty("/supplierInputKey");
-           // var vBukrs = this.getView().byId('client').getSelectedKey();
+            // var vBukrs = this.getView().byId('client').getSelectedKey();
             var vVblnr = this.getView().byId('docno').getValue();
 
             if (vLifnr != null && vLifnr != "") {
@@ -66,16 +89,18 @@ sap.ui.define([
 
             if (bContinue) {
 
+                //
+
                 var url = "/EPPLSTSet?$filter= Ioption eq '2' and Lifnr eq '" + vLifnr + "'";
 
                 if (vVblnr != "") {
                     url += " and Ivblnr eq '" + vVblnr + "'";
                 }
 
-            /*    if (vBukrs != "") {
-                    url += " and IBukrs eq '" + vBukrs + "'";
-                }*/
-                
+                /*    if (vBukrs != "") {
+                        url += " and IBukrs eq '" + vBukrs + "'";
+                    }*/
+
                 if (startDate != "" && endDate != "") {
                     url += " and Ilaufdi eq '" + startDate + "' and Ilaufdf eq '" + endDate + "'";
                 }
@@ -138,16 +163,226 @@ sap.ui.define([
 
             var docResult = results[line];
 
-            this.getOwnerComponent().getRouter().navTo("detailPayPlan",
-                {
-                    layout: sap.f.LayoutType.TwoColumnsMidExpanded,
-                    document: docResult.Vblnr,
-                    laufd: docResult.Laufd,
-                    laufi: docResult.Laufi,
-                    zbukr: docResult.Zbukr,
-                    lifnr: docResult.Lifnr
-                }, true);
-        }
+            this.getOwnerComponent().getRouter().navTo("detailPayPlan", {
+                layout: sap.f.LayoutType.TwoColumnsMidExpanded,
+                document: docResult.Vblnr,
+                laufd: docResult.Laufd,
+                laufi: docResult.Laufi,
+                zbukr: docResult.Zbukr,
+                lifnr: docResult.Lifnr
+            }, true);
+        },
+
+
+
+
+        //handle sech data 
+        searchData: function () {
+
+			//let dateRange = this.getView().byId("dateRange");
+
+			//ciltro documento 
+			 
+
+			let proveedor_LIFNR = this.getConfigModel().getProperty("/supplierInputKey");
+			// format[AAAAMMDD] (2020101)
+			// let desde_LV_ZDESDE = this.buildSapDate( dateRange.getDateValue()       ); 
+			// format[AAAAMMDD] (2020101)
+			// let desde_LV_ZHASTA = this.buildSapDate( dateRange.getSecondDateValue() );
+
+
+
+			//tomar valores dummy para hacer al consulta 
+			let todayDate = new Date();
+
+			// format[AAAAMMDD] (2020101)
+			let desde_LV_ZDESDE =  '20160219'// this.buildSapDate(todayDate);
+			// format[AAAAMMDD] (2020101)
+			let desde_LV_ZHASTA = this.buildSapDate(todayDate);
+
+
+
+			let doc_BELNR = this._document// documentoInput.getValue();
+
+			//checbox validaciones
+
+ 
+		 
+
+
+			if (proveedor_LIFNR == null || proveedor_LIFNR == "") {
+				sap.m.MessageBox.error("El campo proveedor es obligatorio.");
+				return false;
+			}
+
+			if (desde_LV_ZDESDE == "" || desde_LV_ZHASTA == "") {
+				// sap.m.MessageBox.error("Por favor defina el rango de fechas.");
+			}
+
+
+			 
+			var queryFiltro = ""
+ 
+
+
+			var oODataJSONModel = this.getOdata(sUri);
+			//            let urlParams = `EStmtHdrSet?$expand=Citms,Oitms&$filter= Lifnr eq '${proveedor_LIFNR}' and Datei eq '${desde_LV_ZDESDE}' and Datef eq '${desde_LV_ZHASTA}' and belnr eq '${doc_BELNR}'  &$format=json`;
+
+			let urlParams = `EStmtHdrSet?$expand=Citms,Oitms&$filter= Lifnr eq '${proveedor_LIFNR}' and Datei eq '${desde_LV_ZDESDE}' and Datef eq '${desde_LV_ZHASTA}' ${queryFiltro} &$format=json`;
+			//Xblnr
+
+			var odTJSONModel = this.getOdataJsonModel(urlParams, oODataJSONModel);
+			dTJSON = odTJSONModel.getJSON();
+			var TDatos = JSON.parse(dTJSON);
+
+			let Detalles = [...TDatos.results[0].Oitms.results];
+
+			TDatos.results[0].Detalles = {
+				results: [...Detalles]
+			};
+
+			delete TDatos.results[0].Citms;
+			delete TDatos.results[0].Oitms;
+
+
+			//TDatos.results[0].periodo = "Del " + this.formatDateTime(dateRange.getDateValue(), 'dd/MM/YYYY') + " al " + this.formatDateTime(dateRange.getSecondDateValue(), 'dd/MM/YYYY');
+
+
+			var JSONT = $.extend({}, TDatos.results[0]);
+			var jsonModelT = new JSONModel();
+			jsonModelT.setData(JSONT);
+
+
+
+			//filtrar totales y crear modelo grupal 
+
+			console.info("agrupando datos", Detalles)
+			let auxArray = [...Detalles]
+
+
+			var groupedMovs = this.groupArrayOfObjects(auxArray, "FechaTesoreria");
+			var nestedMovs = []
+
+			var me = this;
+
+            var acumulado = 0;
+			for (let x in groupedMovs) {
+
+
+				console.log("sumando valores");
+
+
+				var resultCredit = groupedMovs[x].reduce(function (_this, val) {
+					//console.log(val.Wrbtr)
+					var current = val.Bschl === "21" ? Number(val.Wrbtr) : 0
+					var total = _this + current
+					return me.truncate(total, 2)
+				}, 0);
+
+				//console.log(result)
+
+				var result = groupedMovs[x].reduce(function (_this, val) {
+					var current = val.Bschl !== "21" ? Number(val.Wrbtr) : 0
+					var total = _this + current
+					return me.truncate(total, 2)
+				}, 0);
+
+				var cost = groupedMovs[x].reduce(function (_this, val) {
+					var current =   Number(val.Wrbtr)  
+					var total = _this + current
+					return me.truncate(total, 2)
+				}, 0);
+
+                acumulado=acumulado+Math.abs(cost)
+                console.log(me.truncate(acumulado,2))
+
+				nestedMovs.push({
+					"name": x,
+					"totalRegs": groupedMovs[x].length,
+					"totalDebit": Math.abs(result),
+					"totalCredit": Math.abs(resultCredit),
+					"cost": Math.abs(cost),
+					"positions": groupedMovs[x],
+                    "costoAcumulado": me.truncate(acumulado,2)
+
+				})
+
+
+			}
+
+
+			console.log(nestedMovs);
+
+			var totalR = nestedMovs.reduce(function (_this, val) {
+				var current = Number(val.totalRegs)
+				var total = _this + current
+				return me.truncate(total, 2)
+			}, 0);
+
+			var totalD = nestedMovs.reduce(function (_this, val) {
+				var current = Number(val.totalDebit)
+				var total = _this + current
+				return me.truncate(total, 2)
+			}, 0);
+
+			var totalC = nestedMovs.reduce(function (_this, val) {
+				var current = Number(val.totalCredit)
+				var total = _this + current
+				return me.truncate(total, 2)
+			}, 0);
+
+			var totalCostos = nestedMovs.reduce(function (_this, val) {
+				var current = Number(val.cost)
+				var total = _this + current
+				return me.truncate(total, 2)
+			}, 0);
+
+
+
+
+			var jsonModelG = new JSONModel({
+				"Hierarchy": {
+					"movimientos": nestedMovs,
+					"totalR": totalR,
+					"totalD": totalD,
+					"totalC": totalC,
+					"totalCostos": totalCostos
+
+				}
+			});
+
+			console.log(jsonModelG);
+
+			this.getOwnerComponent().setModel(jsonModelG, "GroupedTotales");
+
+
+			//this.initTable()
+
+			//this.getOwnerComponent().setModel(jsonModelT, "totales");
+
+			//this.paginate("totales", "/Detalles", 1, 0);
+
+		},
+
+
+        subtractYears: function (numOfYears, date = new Date()) {
+            date.setFullYear(date.getFullYear() - numOfYears);
+          
+            return date;
+          },
+          
+          
+           groupArrayOfObjects: function(list, key) {
+            return list.reduce(function(rv, x) {
+              (rv[x[key]] = rv[x[key]] || []).push(x);
+              return rv;
+            }, {});
+          },
+          
+          
+
+
+
 
     });
 });
