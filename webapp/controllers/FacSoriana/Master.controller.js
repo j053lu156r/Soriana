284156314c411a6b4461ff81ba1e5e1064296cc3
Carlos Oliver Hernández Montejano <charlsoliver96@gmail.com>
2022-06-28 18:21:28 +0000

    fix: Traduccion de textos harcodeados

--- webapp/controllers/FacSoriana/Master.controller.js
+++ webapp/controllers/FacSoriana/Master.controller.js
@@ -50,7 +50,7 @@ sap.ui.define([
             if (vLifnr != null && vLifnr != "") {
                 bContinue = true;
             } else {
-                sap.m.MessageBox.error("El campo proveedor es obligatorio.");
+                sap.m.MessageBox.error(this.getOwnerComponent().getModel("appTxts").getProperty("/global.supplierSelectError"));
             }
 
             if (bContinue) {
@@ -59,7 +59,7 @@ sap.ui.define([
                         bContinue = true;
                     } else {
                         bContinue = false;
-                        sap.m.MessageBox.error("Debe ingresar al menos un criterio de busqueda.");
+                        sap.m.MessageBox.error(this.getOwnerComponent().getModel("appTxts").getProperty("/global.searchFieldsEmpty"));
                     }
                 } else {
                     bContinue = true;
