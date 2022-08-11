8a3fd1afa7eb427c691d64d00f2e9a170ee5ed4d
Miguel Parra <mparra190@gmail.com>
2022-08-11 00:06:33 +0000

    BusyIndicator Incidencias

--- webapp/controllers/Incidencias/Master.controller.js
+++ webapp/controllers/Incidencias/Master.controller.js
@@ -38,51 +38,90 @@ sap.ui.define([
 
             if (bContinue) {
 
-                
-                var url = "HdrInSet?$expand=ETLOGFNAV,ETTLIFNAV,ETCFDINAV&$filter= IOption eq '1'"; // Se debe validar que el usuario este activo
+
+                let filtros = [];
+
+                filtros.push(new sap.ui.model.Filter({
+                    path: "IOption",
+                    operator: sap.ui.model.FilterOperator.EQ,
+                    value1: "1"
+                })
+                );
 
                 if (vLifnr != null && vLifnr != "") {
-                    url += " and ILifnr eq '" + vLifnr + "'";
+
+                    filtros.push(new sap.ui.model.Filter({
+                        path: "ILifnr",
+                        operator: sap.ui.model.FilterOperator.EQ,
+                        value1: vLifnr
+                    })
+                    );
                 }
 
                 if (startDate != null && startDate != "" && endDate != null && endDate != "") {
-                    url += " and ISdate eq '" + startDate + "'"
-                    url += " and IFdate eq '" + endDate + "'"
+
+                    filtros.push(new sap.ui.model.Filter({
+                        path: "ISdate",
+                        operator: sap.ui.model.FilterOperator.EQ,
+                        value1: startDate
+                    })
+                    );
+
+                    filtros.push(new sap.ui.model.Filter({
+                        path: "IFdate",
+                        operator: sap.ui.model.FilterOperator.EQ,
+                        value1: endDate
+                    })
+                    );
                 }
 
                 if (uuid != null && uuid != "") {
-                    url += "and IUuid eq '" + uuid + "'";
+
+                    filtros.push(new sap.ui.model.Filter({
+                        path: "IUuid",
+                        operator: sap.ui.model.FilterOperator.EQ,
+                        value1: uuid
+                    })
+                    );
                 }
 
                 if (status != null && status != "") {
-                    url += " and IStatus eq '" + status + "'";
+
+                    filtros.push(new sap.ui.model.Filter({
+                        path: "IStatus",
+                        operator: sap.ui.model.FilterOperator.EQ,
+                        value1: uustatusid
+                    })
+                    );
                 }
 
-                if (vLifnr == null || vLifnr == ""){
-                    if(startDate == null || startDate == ""){
-                        if (uuid == null || uuid == ""){
-                            if(status == null || status == ""){
+                if (vLifnr == null || vLifnr == "") {
+                    if (startDate == null || startDate == "") {
+                        if (uuid == null || uuid == "") {
+                            if (status == null || status == "") {
                                 bContinue = false;
                             }
                         }
                     }
-                } 
-                
-                if(bContinue){
-                var dueModel = oModel.getJsonModel(url);
-                var ojbResponse = dueModel.getProperty("/results/0");
-                console.log(dueModel);
+                }
 
-                this.getOwnerComponent().setModel(new JSONModel(ojbResponse),
-                    "Incidencias");
+                if (bContinue) {
 
-                this.paginate("Incidencias", "/ETLOGFNAV", 1, 0);
+                    sap.ui.core.BusyIndicator.show();
+                    let that = this;
+                    this._GetODataV2("ZOSP_INDASHBOARD_SRV", "HdrInSet", filtros, ["ETLOGFNAV", "ETTLIFNAV", "ETCFDINAV"], "").then(resp => {
+                        that.getOwnerComponent().setModel(new JSONModel(resp.data.results[0]), "Incidencias");
+                        that.paginate("Incidencias", "/ETLOGFNAV", 1, 0);
+                        that.getSegments();
+                        sap.ui.core.BusyIndicator.hide();
+                    }).catch(error => {
+                        console.error(error);
+                    });
 
-                this.getSegments();
-                 }
-                  else{
-                 sap.m.MessageBox.error(this.getOwnerComponent().getModel("appTxts").getProperty("/global.searchFieldsEmpty")); 
-                  }    
+                }
+                else {
+                    sap.m.MessageBox.error(this.getOwnerComponent().getModel("appTxts").getProperty("/global.searchFieldsEmpty"));
+                }
             }
         },
 
@@ -91,7 +130,7 @@ sap.ui.define([
         },
 
         getSegments: function () {
-            var  oItems = this.getOwnerComponent().getModel("Incidencias").getData();
+            var oItems = this.getOwnerComponent().getModel("Incidencias").getData();
             var Data = {
                 "Segmentos": []
             };
@@ -100,7 +139,7 @@ sap.ui.define([
 
             var rbdIndex = this.getView().byId("rbgInc").getSelectedIndex();
 
-            switch ( rbdIndex){
+            switch (rbdIndex) {
                 case 0:
                     string = "Estatus";
                     break;
@@ -112,12 +151,12 @@ sap.ui.define([
             var groupProvider = this.groupByAuto(oItems.ETLOGFNAV.results, string);
 
             for (const provider in groupProvider) {
-                 var obj = {};
+                var obj = {};
                 obj.provider = provider;
                 obj.value = groupProvider[provider];
                 Data.Segmentos.push(obj);
             }
-            
+
             this.getOwnerComponent().setModel(new JSONModel(Data), "segmentos");
         },
         groupByAuto: function (data, key) {
@@ -133,33 +172,33 @@ sap.ui.define([
 
 
         },
-        exportTopTen: function (){
+        exportTopTen: function () {
             var texts = this.getOwnerComponent().getModel("appTxts");
             var columns = [
                 {
-                    name : texts.getProperty("/incidencias.supplier"),
+                    name: texts.getProperty("/incidencias.supplier"),
                     template: {
                         content: "{Lifnr}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.supplier"),
+                    name: texts.getProperty("/incidencias.supplier"),
                     template: {
                         content: "{Nlifnr}"
                     }
-                },                
+                },
                 {
-                    name : texts.getProperty("/incidencias.error"),
+                    name: texts.getProperty("/incidencias.error"),
                     template: {
                         content: "{N1}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.success"),
+                    name: texts.getProperty("/incidencias.success"),
                     template: {
                         content: "{N3}"
                     }
-                },                
+                },
             ];
             this.exportxls('Incidencias', '/ETTLIFNAV/results', columns);
         },
@@ -167,53 +206,53 @@ sap.ui.define([
             var texts = this.getOwnerComponent().getModel("appTxts");
             var columns = [
                 {
-                    name : texts.getProperty("/incidencias.uuid"),
+                    name: texts.getProperty("/incidencias.uuid"),
                     template: {
                         content: "{Uuid}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.vendor"),
+                    name: texts.getProperty("/incidencias.vendor"),
                     template: {
                         content: "{Lifnr}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.vendor"),
+                    name: texts.getProperty("/incidencias.vendor"),
                     template: {
                         content: "{Nlifnr}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.date"),
+                    name: texts.getProperty("/incidencias.date"),
                     template: {
                         content: "{Fecha}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.hour"),
+                    name: texts.getProperty("/incidencias.hour"),
                     template: {
                         content: "{Hora}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.message"),
+                    name: texts.getProperty("/incidencias.message"),
                     template: {
                         content: "{Mensaje}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.desc1"),
+                    name: texts.getProperty("/incidencias.desc1"),
                     template: {
                         content: "{Descripcion1}"
                     }
                 },
                 {
-                    name : texts.getProperty("/incidencias.status"),
+                    name: texts.getProperty("/incidencias.status"),
                     template: {
                         content: "{Estatus}"
                     }
-                },            
+                },
             ];
             this.exportxls('Incidencias', '/ETLOGFNAV/results', columns);
         }
